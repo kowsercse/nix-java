39a68f390c23829c7cd76024cbd03dd0e23e3055
Kowser <kowsercse@gmail.com>
2014-12-17 14:12:53 -0600

    Initial import

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz
+wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz
