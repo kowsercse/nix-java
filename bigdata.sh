5eda3be7c767b5fc4fcdcd661e29b193e2151803
Kowser <kowsercse@gmail.com>
2015-04-08 18:10:12 -0500

    bigtable

---
+++ bigdata.sh
@@ -0,0 +1,5 @@
+wget http://apache.arvixe.com/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+wget http://apache.mesi.com.ar/hbase/stable/hbase-1.0.0-bin.tar.gz
+
+tar -xvzf hadoop-2.6.0.tar.gz
+tar -xvzf hbase-1.0.0-bin.tar.gz
