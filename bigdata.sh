664edc61639af26be906eb4cd57fe83921c5addf
Kowser <kowsercse@gmail.com>
2015-04-08 18:47:27 -0500

    download few more

--- bigdata.sh
+++ bigdata.sh
@@ -1,5 +1,9 @@
 wget http://apache.arvixe.com/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
 wget http://apache.mesi.com.ar/hbase/stable/hbase-1.0.0-bin.tar.gz
+wget http://www.interior-dsgn.com/apache/hive/hive-1.1.0/apache-hive-1.1.0-bin.tar.gz
+wget http://apache.mirrors.tds.net/pig/pig-0.14.0/pig-0.14.0.tar.gz
 
 tar -xvzf hadoop-2.6.0.tar.gz
 tar -xvzf hbase-1.0.0-bin.tar.gz
+tar -xvzf apache-hive-1.1.0-bin.tar.gz
+tar -xvzf pig-0.14.0.tar.gz
