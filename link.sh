507b9a7618f924a727f1b9592231de7ded74dbda
Kowser <kowsercse@gmail.com>
2015-04-08 23:39:12 -0500

    fixed typo

--- link.sh
+++ link.sh
@@ -6,12 +6,12 @@ declare -A installations
 installations[jdk]=jdk1.8.0_40
 installations[tomcat]=apache-tomcat-8.0.21
 installations[maven]=apache-maven-3.3.1
-installations[groovy]=groovy-binary-2.4.3
+installations[groovy]=groovy-2.4.3
 installations[gradle]=gradle-2.3
 installations[grails]=grails-3.0.1
 installations[hadoop]=hadoop-2.6.0
 installations[hbase]=hbase-1.0.0
-installations[hive]=apache-hive-1.1.0
+installations[hive]=apache-hive-1.1.0-bin
 installations[pig]=pig-0.14.0
 
 cd /opt
