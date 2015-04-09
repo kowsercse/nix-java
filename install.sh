3a57f9b16a9af033f410d05cebec868c4845fcc6
Kowser <kowsercse@gmail.com>
2015-04-08 19:04:24 -0500

    download grails

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ wget http://ftp.wayne.edu/apache/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.2
 wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz
 wget http://dl.bintray.com/groovy/maven/groovy-binary-2.4.3.zip
 wget https://services.gradle.org/distributions/gradle-2.3-bin.zip
+wget https://github.com/grails/grails-core/releases/download/v3.0.1/grails-3.0.1.zip
 
 tar -xvzf jdk-8u40-linux-x64.tar.gz
 tar -xvzf apache-tomcat-8.0.21.tar.gz
 tar -xvzf apache-maven-3.3.1-bin.tar.gz
 unzip groovy-binary-2.4.3.zip
 unzip gradle-2.3-bin.zip
+unzip grails-3.0.1.zip
